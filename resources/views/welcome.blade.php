3d61c44f2e214291cc04d13ce3d05850538d4ef7
Flo Tlm <tailame.florent@gmail.com>
2022-09-17 18:47:35 +0400

    section dernier article

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
 
 
         </div>
-        <div class="text-white pt-10 mt-16">
+        <div class="text-white pt-14 ">
             <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>&nbsp;
             <p> Proin ac turpis vel sem laoreet fermentum at ut dolor. Etiam posuere elit at interdum varius. Fusce lectus
                 quam, ultricies id mi ut, lacinia placerat turpis. Nam efficitur velit in tortor condimentum cursus. Nulla
@@ -36,7 +36,7 @@
         </div>
 
     </article>
-    <section class="max-w-7xl mx-auto overflow-x-auto pb-10">
+    <section class="max-w-7xl mx-auto overflow-x-auto pb-36">
         <h1 class="text-4xl font-bold leading-tight md:text-3xl text-center text-white mt-32 ">Gallerie</h1>
 
         <div class="swiper mySwiper mt-20 ">
@@ -71,27 +71,56 @@
         </div>
     </section>
 
-
-    <section class= "max-w-7xl px-6 py-15 mx-auto space-y-12 pb-10">
-      <div class= "max-w-7xl bg-[#D9D9D9]">
-        <h1 class="text-4xl font-bold leading-tight md:text-3xl text-center text-black mt-32">Dernier article publié</h1>
-        <
+<section class=" max-w-7xl  text-black mx-auto pb-36 ">
+  <div class=" px-5 py-24 mx-auto bg-[#D9D9D9] opacity-80">
+    <h1 class="text-4xl font-bold leading-tight md:text-3xl text-center text-black mb-14">Dernier article</h1>
+    <div class="lg:w-4/5 mx-auto flex flex-wrap">
+      <div class="lg:w-1/2 w-full lg:pr-10 lg:py-6 mb-6 lg:mb-0">
+     
+        <h1 class="text-gray-900 text-3xl title-font font-medium mb-1">Gameplay</h1>
+        <div class="flex">
+        <span class="text-gray-700">319 j'aime</span>
+        <img src="/storage/img/like2.png" alt="" class="w-auto h-5 ml-1 pt-1"/>
+        </div>
+        
        
+       
+        
+     
+        <div class="flex">
+            <div class="flex">
+                <p class="leading-relaxed mb-8 mt-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Proin ac turpis vel sem laoreet fermentum at ut dolor. Etiam posuere elit at interdum varius. Fusce lectus quam, ultricies id mi ut, lacinia placerat turpis. Nam efficitur velit in tortor condimentum cursus. Nulla euismod ut justo non laoreet. Ut cursus vestibulum eros, eu ullamcorper lacus dapibus eu. Nam efficitur velit in tortor condimentum cursus. Nulla euismod ut justo non laoreet. Ut cursus vestibulum eros, eu ullamcorper lacus dapibus eu.Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Proin ac turpis vel sem laoreet fermentum at ut dolor. Etiam posuere elit at interdum varius. Fusce lectus quam, ultricies id mi ut, lacinia placerat turpis. Nam efficitur velit in tortor condimentum cursus. Nulla euismod ut justo non laoreet. Ut cursus vestibulum eros, eu ullamcorper lacus dapibus eu. Nam efficitur velit in tortor condimentum cursus. Nulla euismod ut justo non laoreet. Ut cursus vestibulum eros, eu ullamcorper lacus dapibus eu. </p>
+                </div>
+        
+         
+        
+          
+        </div>
+        <div>
+        <button class="flex justify-between  ml-auto text-black bg-[#F2A341] border-0 py-2 px-6 focus:outline-none hover:bg-[#FFC83C] rounded-full">
+            <span>Voir plus </span>
+            <img src="/storage/img/iconlogin2.png" alt="" class="w-auto h-6 ml-1 pt-0"/>
+        </div>
+        </button>
       </div>
-      
-       
+    
+      <img alt="gameplay" class="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded" src="/storage/img/gameplay1.jpg">
+  
+    </div>
+    
+  </div>
+</section>
 
-
-    </section>
-
-    <!-- Swiper JS -->
+    <!-- Caroussel JS -->
     <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
     <script>
         var swiper = new Swiper('.mySwiper', {
             spaceBetween: 30,
             centeredSlides: true,
             autoplay: {
-                delay: 3000,
+                delay: 2800,
                 disableOnInteraction: false,
             },
             pagination: {
