fdc0e8e5c10c6bd8ef0ea856bef1cdb3a1d7000b
Flo Tlm <tailame.florent@gmail.com>
2022-09-21 21:57:47 +0400

    fix page articles

---
+++ resources/views/composent/articles/articleBloc.blade.php
@@ -0,0 +1,106 @@
+<section class="max-w-7xl p-10  mx-auto  text-black">
+    <article class="max-w-7xl px-6 py-15 mx-auto space-y-12">
+        <div class="w-full mx-auto  text-center">
+            <div class="flex mx-96 pl-16">
+                <img src="/storage/img/mazu_cameleon_transparent_D.png" alt="mazu" class="w-32">
+                <img src="/storage/img/mazu_cameleon_transparent_G.png" alt="mazu" class="w-32">
+            </div>
+            <h1 class="text-4xl font-bold leading-tight md:text-3xl text-white ">Articles</h1>
+
+        </div>
+        <div class="max-w-7xl mx-auto">
+            <div class="grid justify-center grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+                <a rel="noopener noreferrer" href="#"
+                    class=" group hover:no-underline focus:no-underline bg-white">
+                    <img role="presentation" class="object-cover w-full  h-44 bg-white"
+                        src="/storage/img/1080pSS10.jpg">
+                    <div class="p-6 space-y-2">
+                        <h3 class="text-2xl font-semibold group-hover:underline group-focus:underline">Gameplay</h3>
+                        <p class="text-sm text-gray-400 mt-3 ml-1">publié le 09/09/2022</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            Proin ac turpis vel sem laoreet fermentum at ut dolor. Etiam posuere elit at interdum
+                            varius. Fusce lectus quam, ultricies id mi ut, lacinia placerat turpis. Nam efficitur velit
+                            in tortor condimentum cursus. Nulla euismod ut justo non laoreet...</p>
+                    </div>
+
+                    <div class="flex justify-evenly">
+                        <div class="flex flex-start m-6">
+                            <p class=""> 319 </p>
+                            <img src="/storage/img/like.png" alt="like" class="h-5 ml-1">
+                        </div>
+
+                        <div class="flex flex-start m-6">
+                            <p class=""> 319 </p>
+                            <img src="/storage/img/bulle.png" alt="like" class="h-5 ml-1">
+                        </div>
+                        <div class="flex flex-start m-6">
+                            <p class=""> 319 </p>
+                            <img src="/storage/img/share.png" alt="like" class="h-5 ml-1">
+                        </div>
+                    </div>
+
+                </a>
+                <a rel="noopener noreferrer" href="#"
+                    class="   group hover:no-underline focus:no-underline bg-white">
+                    <img role="presentation" class="object-cover w-full  h-44 bg-white"
+                        src="/storage/img/2021-12-26_2.png">
+                    <div class="p-6 space-y-2">
+                        <h3 class="text-2xl font-semibold group-hover:underline group-focus:underline">Les personnages
+                        </h3>
+                        <p class="text-sm text-gray-400 mt-3 ml-1">publié le 09/09/2022</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            Proin ac turpis vel sem laoreet fermentum at ut dolor. Etiam posuere elit at interdum
+                            varius. Fusce lectus quam, ultricies id mi ut, lacinia placerat turpis. Nam efficitur velit
+                            in tortor condimentum cursus. Nulla euismod ut justo non laoreet...
+                    </div>
+
+                    <div class="flex justify-evenly">
+                        <div class="flex flex-start m-6">
+                            <p class=""> 319 </p>
+                            <img src="/storage/img/like.png" alt="like" class="h-5 ml-1">
+                        </div>
+
+                        <div class="flex flex-start m-6">
+                            <p class=""> 319 </p>
+                            <img src="/storage/img/bulle.png" alt="like" class="h-5 ml-1">
+                        </div>
+                        <div class="flex flex-start m-6">
+                            <p class=""> 319 </p>
+                            <img src="/storage/img/share.png" alt="like" class="h-5 ml-1">
+                        </div>
+                    </div>
+                </a>
+                <a rel="noopener noreferrer" href="#"
+                    class="  group hover:no-underline focus:no-underline bg-white">
+                    <img role="presentation" class="object-cover w-full  h-44 bg-white"
+                        src="/storage/img/nouvelle_map_wip.png">
+                    <div class="p-6 space-y-2">
+                        <h3 class="text-2xl font-semibold group-hover:underline group-focus:underline">La carte</h3>
+                        <p class="text-sm text-gray-400 mt-3 ml-1">publié le 09/09/2022</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            Proin ac turpis vel sem laoreet fermentum at ut dolor. Etiam posuere elit at interdum
+                            varius. Fusce lectus quam, ultricies id mi ut, lacinia placerat turpis. Nam efficitur velit
+                            in tortor condimentum cursus. Nulla euismod ut justo non laoreet...</p>
+                    </div>
+
+                    <div class="flex justify-evenly">
+                        <div class="flex flex-start m-6">
+                            <p class=""> 319 </p>
+                            <img src="/storage/img/like.png" alt="like" class="h-5 ml-1">
+                        </div>
+
+                        <div class="flex flex-start m-6">
+                            <p class=""> 319 </p>
+                            <img src="/storage/img/bulle.png" alt="like" class="h-5 ml-1">
+                        </div>
+                        <div class="flex flex-start m-6">
+                            <p class=""> 319 </p>
+                            <img src="/storage/img/share.png" alt="like" class="h-5 ml-1">
+                        </div>
+                    </div>
+                </a>
+
+            </div>
+        </div>
+    </article>
+</section>
